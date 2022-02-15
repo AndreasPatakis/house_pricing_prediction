d883565aa6a960117c274bbb8ec9d78739a6e945
andreaspatakis <BinaryHubTLM123$>
2022-02-15 19:28:36 +0200

    preprocess completed

---
+++ preprocess/data_preprocess.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+def df_column_to_one_hot_vector(df):
+    """Returns a dataframe of each value in OneHotVector representation"""
+    column_unique = list(df.drop_duplicates())
+    template_vector = np.zeros(len(column_unique),dtype=int)
+    dict = {}
+    for i,entry in enumerate(column_unique):
+        dict[entry] = template_vector.copy()
+        dict[entry][i] = 1
+    return dict
+
+def normalize(df,features):
+    """Using MinMaxScaler to normalize our data"""
+    scaler = MinMaxScaler()
+    df[features] = scaler.fit_transform(df[features])
+    return df
+
+def standarize(df,features):
+    """Using Standard Scaler to standarize data"""
+    scaler = StandardScaler()
+    df[features] = scaler.fit_transform(df[features])
+    return df
+
+
+if __name__ == "__main__":
+
+    DATA_PATH = './housing.csv'
+
+    df = pd.read_csv(DATA_PATH)
+    num_of_records = df.shape[0]
+    pd.set_option('display.max_columns', None)
+    print("\nOur dataframe currently: \n\n{}".format(df.head()))
+
+    #pandas wraps 'string' type as 'object' type, so in this way we will find the categorical data
+    numerical_attr = list(df.select_dtypes(exclude='object').columns.copy())
+    numerical_attr_str = ', '.join(numerical_attr)
+    categ_attr = list(df.select_dtypes(include='object').columns.copy())
+    categ_attr_str = ', '.join(categ_attr)
+    print("\nColumns with numerical data are: {}".format(numerical_attr_str))
+    print("\nColumns with categorical data are: {}".format(categ_attr_str))
+
+
+    """HANDLING CATEGORICAL DATA"""
+    print("\n-----Processing categorical data-----")
+    #We choose to drop all categorical entries that dont have a value
+    print("\nDropping categorical records with no value...")
+    df.dropna(subset=categ_attr,inplace=True)
+    print('{} records have been dropped.'.format(num_of_records-df.shape[0]))
+
+    for categ_column in categ_attr:
+        categ_dict = df_column_to_one_hot_vector(df[categ_column])
+        new_columns = list(categ_dict.keys())
+        df[new_columns] = np.nan
+        for key in categ_dict.keys():
+            df.loc[df[categ_column]==key,new_columns] = categ_dict[key]
+        df.drop([categ_column],axis=1,inplace=True)
+        print("\n'{}' categorical attribute replaced by attributes '{}' with resprect to OneHotVector representation.".format(categ_column,', '.join(new_columns)))
+
+
+
+    """HANDLING NUMEIRICAL DATA"""
+    print("\n-----Processing numerical data-----")
+    #replacing empty records with the attribute's median value
+    for col in numerical_attr:
+        na_values = df[col].isna().sum()
+        print("\nAttribute '{}' has {} empty records.".format(col,na_values))
+        if na_values:
+            attr_median = df[col].median()
+            df[col].fillna(attr_median, inplace=True)
+            print('Records got replaced by the median value {}.'.format(attr_median))
+
+    print('\n--Scaling data--')
+    print('\nNormalization...',end='')
+    df_norm = normalize(df,numerical_attr).copy()
+    print('DONE')
+
+    print('Standarization...',end='')
+    df_stand = standarize(df,numerical_attr).copy()
+    print('DONE')
+
+    print('\n--Exporting csv files--')
+    df_norm.to_csv('./housing_normalized.csv')
+    df_stand.to_csv('./housing_standarized.csv')
+    print('Exporting completed successfully!')
